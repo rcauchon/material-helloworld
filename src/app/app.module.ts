fe89b86e6eb612f35325152f3bc819cf4249e6e9
Remi Cauchon <rcauchon@ca.ibm.com>
2019-05-16 13:46:04 -0400

    Ajouter le titre LGS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { MaterialModule } from './material/material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { ButtonTypesExampleComponent } from './button-types-example/button-types-example.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ButtonTypesExampleComponent
   ],
   imports: [
     BrowserModule,
